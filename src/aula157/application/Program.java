e1909af9665f778a512192c433f5a544cd3ae3fa
Breno de Freitas <brenofreitas28f@gmail.com>
2024-07-26 17:39:09 -0300

    Aprendendo sobre herança

---
+++ src/aula157/application/Program.java
@@ -0,0 +1,11 @@
+package aula157.application;
+
+import aula157.entities.BusinessAccount;
+
+public class Program {
+    public static void main(String[] args) {
+        BusinessAccount account = new BusinessAccount();
+
+        account.getBalance();
+    }
+}
