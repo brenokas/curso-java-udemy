222bfe76077634cb454b33bdef0017a8cbe821dd
Breno de Freitas Farias <brenofreitas28f@gmail.com>
2024-07-30 22:17:00 -0300

    Pequena correção no diretorio do arquivo

--- src/aula173/app/Program.java
+++ src/aula173/app/Program.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
 
 public class Program {
     public static void main(String[] args) {
-        File file = new File("home\\brenokas\\Documents\\in.txt");
+        File file = new File("/home/brenokas/Documents/in.txt");
         Scanner sc = null;
 
         try {
